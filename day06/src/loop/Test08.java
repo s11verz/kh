902eef9dd0aac8644a60e30eb782e209de04ecd7
silverZZ0124 <72031018+silverZZ0124@users.noreply.github.com>
2021-03-10 10:35:39 +0900

    day06
    과제 수정

--- day06/src/loop/Test08.java
+++ day06/src/loop/Test08.java
@@ -8,8 +8,9 @@ public class Test08 {
 		int total=0;
 		for(int i=1;i<=30;i++) {
 			System.out.println(i+"일날 받을 금액은"+pay+"원 입니다.");
-			pay*=2;
 			total+=pay;
+			pay*=2;
+			
 		}
 		System.out.println("30일 동안의 총 금액은"+total+"원 입니다.");
 
