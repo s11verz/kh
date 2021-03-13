ee802d8a401dde0cb895e0df3bddf7a4653dfd3f
silverZZ0124 <72031018+silverZZ0124@users.noreply.github.com>
2021-03-13 22:20:14 +0900

    day 02
    실습

---
+++ day02/src/Hello.java
@@ -0,0 +1,7 @@
+
+public class Hello {
+public static void main(String[] args) {
+	System.out.println("Hello, java!");
+	System.out.println("¾È³ç, ÀÚ¹Ù!");
+}
+}
---
+++ day02/src/data/Test02.java
@@ -0,0 +1,15 @@
+package data;
+
+import java.lang.*;
+
+public class Test02 {
+public static void main(String[] args) {
+	int a=5000; //Â¥Àå¸é
+	int b=6000; //Â«»Í
+	System.out.println(a*2);
+	System.out.println(b*3);
+	System.out.println(a*2+b*3);
+	System.out.println((a*2+b*3)-2000);
+	
+}
+}
