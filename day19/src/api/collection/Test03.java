79fea01a93e7d5b29df45ad20562979edb1d3728
silverZZ0124 <72031018+silverZZ0124@users.noreply.github.com>
2021-03-29 12:15:39 +0900

    day19
    과제 수정

--- day19/src/api/collection/Test03.java
+++ day19/src/api/collection/Test03.java
@@ -8,18 +8,36 @@ public class Test03 {
 
 	public static void main(String[] args) {
 		Random r = new Random();
-		ArrayList a = new ArrayList();
+		ArrayList<Integer> a = new ArrayList<>();
+		
 
 		for (int i = 0; i < 6; i++) {
 			a.add(r.nextInt(45) + 1);
 			
 			for (int j = 0; j < i; j++) {
-				if (a.get(i) == a.get(j))
+				if (a.get(i)==a.get(j)) {
+					a.remove(j);
 					i--;
+					break;
+				}
 
 			}
+			
 
 		}
+/* 성공한 코드
+  		int i=0;
+  		
+		while(i<7) {
+			int num=r.nextInt(45)+1;
+			
+			if(!a.contains(num)) {
+				a.add(num);
+				i++;
+			}else {
+				continue;
+			}
+		}*/
 
 		System.out.println(a);
 
