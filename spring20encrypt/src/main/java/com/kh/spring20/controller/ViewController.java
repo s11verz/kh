354bd5faa6a51b26cd53004c5a1b36b94525d9d5
lisa9601 <liisa9601@naver.com>
2021-07-02 16:32:43 +0900

    프론트엔드 SHA256 암호화

---
+++ spring20encrypt/src/main/java/com/kh/spring20/controller/ViewController.java
@@ -0,0 +1,13 @@
+package com.kh.spring20.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+}
