fcb255741c363eb143354d606a05d42fd716c819
lisa9601 <liisa9601@naver.com>
2021-07-05 11:54:11 +0900

    결제 준비 요청 테스트 성공

--- spring21pay/src/test/java/com/kh/spring21/Test02.java
+++ spring21pay/src/test/java/com/kh/spring21/Test02.java
@@ -2,6 +2,7 @@ package com.kh.spring21;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.UUID;
 
 import org.junit.Test;
 import org.springframework.http.HttpEntity;
@@ -34,7 +35,17 @@ public class Test02 {
 		
 		//[3] Http Body 생성(ex : 편지내용)
 		MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-		body.add("이름", "데이터");
+		body.add("cid", "TC0ONETIME");//가맹점ID(사업자 제휴 시 발급, 테스트용 ID 사용)
+		body.add("partner_order_id", UUID.randomUUID().toString());//판매자 구분 ID
+		body.add("partner_user_id", UUID.randomUUID().toString());//구매자 구분 ID
+		body.add("item_name", "강남 50평 아파트");//명세서에 표시될 구매 내역(다건 구매일 경우 ???외 ?건 형식)
+		body.add("quantity", "3");//구매수량
+		body.add("total_amount", "999900");//총 구매금액
+		body.add("tax_free_amount", "0");//면세금액(없으면 0)
+		
+		body.add("approval_url", "http://localhost:8080/spring21/pay/success");//사용자 결제 성공시 신호를 받을 주소
+		body.add("cancel_url", "http://localhost:8080/spring21/pay/cancel");//사용자 결제 취소시 신호를 받을 주소
+		body.add("fail_url", "http://localhost:8080/spring21/pay/fail");//사용자 결제 실패시 신호를 받을 주소
 		
 		//[4] Http Header / Body 합성
 		HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(body, headers);
